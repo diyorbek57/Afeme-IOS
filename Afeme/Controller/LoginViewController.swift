8db66cce3571388a298b1661ae788392bd4fa9e5
Diyorbek <hosmerdiyir57@gmail.com>
2022-05-31 15:58:14 +0500

    //

---
+++ Afeme/Controller/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Afeme
+//
+//  Created by Diyorbek Hamdamov on 31/05/22.
+//
+
+import UIKit
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
